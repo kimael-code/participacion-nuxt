d29fa3650621a84a6d69fb78ce6481eacdad2f96
Maikel Carballo <mcarballo@tutanota.com>
2025-11-06 21:29:03 -0400

    build: update `eslint.config.mjs`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,7 @@
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
-export default withNuxt(
-  // your custom flat configs go here, for example:
-  // {
-  //   files: ['**/*.ts', '**/*.tsx'],
-  //   rules: {
-  //     'no-console': 'off' // allow console.log in TypeScript files
-  //   }
-  // },
-  // {
-  //   ...
-  // }
-)
+export default withNuxt().override('nuxt/typescript/rules', {
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+});
